6161cad678243a3e1fd70a7899e74024464e0464
DracoR22 <rdraco039@gmail.com>
2024-03-25 15:55:33 -0500

    landing page done

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,20 @@
+import InfoBar from "@/components/infobar"
+import Sidebar from "@/components/sidebar"
+
+interface Props {
+    children: React.ReactNode
+}
+
+const Layout = ({ children }: Props) => {
+    return (
+        <div className="flex overflow-hidden h-screen">
+            <Sidebar/>
+             <div className="w-full">
+                <InfoBar/>
+               {children}
+             </div>
+        </div>
+    )
+}
+
+export default Layout
