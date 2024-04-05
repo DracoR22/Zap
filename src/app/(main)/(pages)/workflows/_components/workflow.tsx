9403bc6070873d01919a6417edf833513709652d
DracoR22 <rdraco039@gmail.com>
2024-04-04 22:29:13 -0500

    editor completed

--- src/app/(main)/(pages)/workflows/_components/workflow.tsx
+++ src/app/(main)/(pages)/workflows/_components/workflow.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image'
 import { Label } from '@/components/ui/label'
 import { Switch } from '@/components/ui/switch'
 import { toast } from 'sonner'
+import { onFlowPublish } from '../_actions/workflow-connections'
 
 type Props = {
   name: string
@@ -15,11 +16,11 @@ type Props = {
 
 const Workflow = ({ description, id, name, publish }: Props) => {
   const onPublishFlow = async (event: any) => {
-    // const response = await onFlowPublish(
-    //   id,
-    //   event.target.ariaChecked === 'false'
-    // )
-    // if (response) toast.message(response)
+    const response = await onFlowPublish(
+      id,
+      event.target.ariaChecked === 'false'
+    )
+    if (response) toast.message(response)
   }
 
   return (
@@ -42,7 +43,7 @@ const Workflow = ({ description, id, name, publish }: Props) => {
           {publish! ? 'On' : 'Off'}
         </Label>
         <Switch id="airplane-mode"
-          // onClick={onPublishFlow}
+          onClick={onPublishFlow}
           defaultChecked={publish!}
         />
       </div>
